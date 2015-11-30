88b1f8d84d505f1353b2705b34d725755f3f3048
Amie Phipps <amiejoyphipps@gmail.com>
2015-11-30 16:33:24 -0500

    changes

--- page-services.php
+++ page-services.php
@@ -6,11 +6,32 @@
 			<div class="servicesHeader animated fadeIn">
 		      <h4>Services</h4>
 
-				<h5>I care about the quality of design and code I provide.</h5>
-				<h5>It isn't just about a design that looks good, you receive a fully responsive functional website that is based on your business objectives and gets results.</h5>
+				<p>I can help you with every stage of your digital project. Everything I creat is done so with the business objectives of your project in mind. Before any code is written, we start out with a plan. <a href="#contact" class="smoothScroll">Contact me</a> today to get started on your plan.</p>
 			</div> <!-- servicesHeader -->
 			
 			<div class="servicesLists animated fadeIn">
+				<div class="typeOfService">
+						<img src="<?php bloginfo('template_url'); ?>/images/design.svg" alt="image of a layout">
+					
+					<h6>Design.</h6>
+					<p>We work together to get a design that works for you and your business.</p>
+				</div>
+				<div class="typeOfService">
+					<div>
+						<img src="<?php bloginfo('template_url'); ?>/images/development.svg" alt="iamge of a computer to symbolize ">
+					</div>
+					<h6>Development.</h6>
+					<p>We work together to get a design that works for you and your business.</p>
+				</div>
+				<div class="typeOfService">
+					<div>
+						<i class="fa fa-wordpress fa-5x"></i>
+					</div>
+					<h6>WordPress.</h6>
+					<p>We work together to get a design that works for you and your business.</p>
+				</div>
+	
+<!-- 
 				<div class="servicesList">
 					<h6>What you can expect:</h6>
 					<ul>
@@ -32,7 +53,7 @@
 						<li>User Experience</li>
 						<li>HTML Emails</li>
 					</ul>
-				</div>
+				</div> -->
 			</div> <!-- servicesList -->
 
 		</div> <!-- WRAPPER -->
