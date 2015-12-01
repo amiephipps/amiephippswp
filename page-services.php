143ab922165214d4191dd4d81b56d725f1a792ab
Amie Phipps <amiejoyphipps@gmail.com>
2015-12-01 14:41:51 -0500

    changes

--- page-services.php
+++ page-services.php
@@ -3,58 +3,87 @@
 <div class="rightSide">
    <main class="services">
    	<div class="wrapper">
-			<div class="servicesHeader animated fadeIn">
-		      <h4>Services</h4>
 
+   		<h4>Services</h4>
+
+	   	<section class="process">
+	   		<h3>Here is what you can expect from start to finish...</h3>
+	   		<div class="dashedLine"></div>
+	   		<ul>
+	   			<li>
+	   				<i class="fa fa-paint-brush fa-3x"></i>
+	   				<div class="processPlaceHolder"></div>
+	   				<div class="point"></div>
+	   				<div class="processContent">
+	   					<h6>Inquiry.</h6>
+	   					<p>Once you <a href="#contact">contact me</a> we will get an initial converstaion going, discuss ideas, business objectives, requirements and desires.</p>
+	   				</div>
+	   			</li>
+	   			<li>
+	   				<div class="processContent">
+	   					<h6>Discussion.</h6>
+	   					<p>I will send you a proposal/estimate. Once approved, you'll receive a contract. Discussions are held throughout this time to ensure we are always on the same page.</p>
+	   				</div>
+	   				<div class="point"></div>
+	   				<div class="processPlaceHolder"></div>
+	   				<i class="fa fa-wrench fa-3x"></i>
+	   			</li>
+	   			<li>
+	   				<i class="fa fa-cog fa-3x"></i>
+	   				<div class="processPlaceHolder"></div>
+	   				<div class="point"></div>
+	   				<div class="processContent">
+	   					<h6>Design &amp; Develop.</h6>
+	   					<p>Once all is signed and ready, the real work begins! You will approve a design and I will get coding!</p>
+	   				</div>
+	   			</li>
+	   			<li>
+	   				<div class="processContent">
+	   					<h6>Review &amp; Deploy.</h6>
+	   					<p>When the website is completed, I will provide you with a link so you can review and test it. I will implement any changes, and we will oficially deploy.</p>
+	   				</div>
+	   				<div class="point"></div>
+	   				<div class="processPlaceHolder"></div>
+	   				<i class="fa fa-cloud-upload fa-3x"></i>
+	   			</li>
+	   		</ul>
+	   	</section>
+
+			<section class="servicesHeader animated fadeIn">
 				<p>I can help you with every stage of your digital project. Everything I creat is done so with the business objectives of your project in mind. Before any code is written, we start out with a plan. <a href="#contact" class="smoothScroll">Contact me</a> today to get started on your plan.</p>
-			</div> <!-- servicesHeader -->
+			</section> <!-- servicesHeader -->
 			
-			<div class="servicesLists animated fadeIn">
+			<section class="servicesLists animated fadeIn">
 				<div class="typeOfService">
-						<img src="<?php bloginfo('template_url'); ?>/images/design.svg" alt="image of a layout">
-					
+					<i class="fa fa-desktop fa-5x"></i>
 					<h6>Design.</h6>
-					<p>We work together to get a design that works for you and your business.</p>
+					<p>Not sure what you want your site to look like? Don't panic! Let me help you discover the story your website will tell. We work together to get a unique and create design that works for you and your business.</p>
 				</div>
 				<div class="typeOfService">
-					<div>
-						<img src="<?php bloginfo('template_url'); ?>/images/development.svg" alt="iamge of a computer to symbolize ">
-					</div>
+					<i class="fa fa-code fa-5x"></i>
 					<h6>Development.</h6>
-					<p>We work together to get a design that works for you and your business.</p>
+					<p>Using the best practices in the industry, I build your website using responsive HTML5, CSS3, Sass, JavaScript, jQuery, Git, Gulp, Trello, and any other technologies required to bring your website to life. </p>
 				</div>
 				<div class="typeOfService">
-					<div>
-						<i class="fa fa-wordpress fa-5x"></i>
-					</div>
+					<i class="fa fa-wordpress fa-5x"></i>
 					<h6>WordPress.</h6>
-					<p>We work together to get a design that works for you and your business.</p>
+					<p>For those looking to have complete control over their future content updates, I build completely customized WordPress themes.</p>
 				</div>
-	
-<!-- 
-				<div class="servicesList">
-					<h6>What you can expect:</h6>
-					<ul>
-						<li>Website Development and/or Design</li>
-						<li>Creation of a project plan based on your goals</li>
-						<li>Learning your business goals and objectives</li>
-						<li>Reviews and feedback</li>
-						<li>Training for content management</li>
-						<li>Schedule for future updates</li>
-					</ul>
-				</div>
-
-				<div class="servicesList">
-					<h6>What you get:</h6>
-					<ul>
-						<li>Web Development</li>
-						<li>Custom WordPress Theme</li>
-						<li>Web Design</li>
-						<li>User Experience</li>
-						<li>HTML Emails</li>
-					</ul>
-				</div> -->
-			</div> <!-- servicesList -->
+			</section> <!-- servicesList -->
+				
+			<section class="otherServicesList">
+				<!-- <p>Other Services Available:</p> -->
+				<ul>
+					<li><p><i class="fa fa-check"></i>Learning your business goals and objectives</p></li>
+					<li><p><i class="fa fa-check"></i>Strategic project planning</p></li>
+					<li><p><i class="fa fa-check"></i>Reviews and feedback</p></li>
+					<li><p><i class="fa fa-check"></i>Training for content management</p></li>
+					<li><p><i class="fa fa-check"></i>Schedule for future updates</p></li>
+					<li><p><i class="fa fa-check"></i>SEO optimization</p></li>
+				</ul>
+			</section>
+			
+			
 
 		</div> <!-- WRAPPER -->
  		<?php get_footer(); ?>
