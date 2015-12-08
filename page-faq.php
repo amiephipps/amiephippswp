e4137a01d6663ab422f3c7d6134bd1d09ff6ecb9
Amie Phipps <amiejoyphipps@gmail.com>
2015-12-08 15:02:58 -0500

    faq changes

---
+++ page-faq.php
@@ -0,0 +1,25 @@
+<?php get_header();  ?>
+
+<div class="rightSide">
+   <main class="about"> 
+   	<div class="wrapper">     
+			<div class="relative">
+		      <h4 class="animated fadeIn"><?php the_title(); ?></h4>
+
+		      <div class="aboutImage animated fadeIn">
+		      	<?php the_post_thumbnail('portfolio'); ?>
+		      </div>
+		      
+		      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>     
+		      <div class="aboutContent animated fadeIn">
+		         <p><?php the_content(); ?></p>
+
+		    	 <?php endwhile; // end the loop?>
+
+		   	   <a href="index.php?page_id=57">See My Resume</a>
+		      </div>
+			</div> <!-- relative -->
+	   </div> <!-- wrapper -->
+		<?php get_footer(); ?>
+   </main> 
+   
